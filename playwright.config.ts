549c4f222c0ce5d62cadec8b32b74acdac130360
anilsimha13 <tanilsimha@gmail.com>
2026-04-23 09:50:45 +0530

    initial changes

---
+++ playwright.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig, devices } from "@playwright/test";
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+export default defineConfig({
+  testDir: "./tests",
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [["html", { open: "never" }], ["list"]],
+  use: {
+    trace: "on-first-retry",
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+});
---
+++ tests/api/api-test.spec.ts
@@ -0,0 +1,43 @@
+import { fa, faker } from "@faker-js/faker";
+import { request, test } from "@playwright/test";
+
+const email: string = faker.internet.email();
+const password: string = "Kotoco@123";
+test("Should validate the Registration of a user", async ({ request }) => {
+  const response = await request.post(
+    "https://api.practicesoftwaretesting.com/users/register",
+    {
+      data: {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        dob: "2001-01-01",
+        phone: "6281227715",
+        email: email,
+        password: password,
+        address: {
+          street: faker.location.streetAddress(),
+          city: faker.location.city(),
+          state: faker.location.state(),
+          country: faker.location.country(),
+          postal_code: faker.location.zipCode(),
+        },
+      },
+    },
+  );
+  const response_json = await response.json();
+  console.log(response_json);
+});
+
+test("Should Login user into the application", async ({ request }) => {
+  const response = await request.post(
+    "https://api.practicesoftwaretesting.com/users/login",
+    {
+      data: {
+        email: email,
+        password: password,
+      },
+    },
+  );
+  const response_json = await response.json();
+  console.log(response_json);
+});
